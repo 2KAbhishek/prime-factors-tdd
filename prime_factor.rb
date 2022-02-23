45dcd6412e15f44cd0c4758cf1e36186c92f5134
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-02-24 03:22:02 +0530

    Use modifier while

--- prime_factor.rb
+++ prime_factor.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
-# Generate prime factors of a number
+# Generate list of prime factors of a number
 class PrimeFactor
   def factor_of(number)
     factors = []
     divisor = 2
     while number > 1
-      while (number % divisor).zero?
-        factors.push(divisor)
-        number /= divisor
-      end
+      factors.push(divisor) && number /= divisor while (number % divisor).zero?
       divisor += 1
     end
     factors
